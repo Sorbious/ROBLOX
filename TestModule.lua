76ffa97611b48144447276a758c516b7e8c7e38c
Sorbious <87640739+Sorbious@users.noreply.github.com>
2021-07-19 18:10:43 +0800

    Update TestModule.lua

--- TestModule.lua
+++ TestModule.lua
@@ -8,4 +8,19 @@ function module.request(request, value)
   end
 end
 
+function module:KillPlayer(player)
+  local Players = game:GetService("Players")
+  if Players:FindFirstChild(player) then
+    local Character = player.Character
+    local Humanoid = Character:FindFirstChild("Humanoid")
+    if Humanoid then
+      Humanoid:TakeDamage(100)
+    else
+      warn("Humanoid not found.")
+    end
+  else
+    warn("Player not found.")
+  end
+end
+
 return module
