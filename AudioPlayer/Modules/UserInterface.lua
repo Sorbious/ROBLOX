df516fb2d5912c6bfaa8e315774108d0277aa725
Sorbious <87640739+Sorbious@users.noreply.github.com>
2021-07-21 18:35:15 +0800

    Create UserInterface.lua

---
+++ AudioPlayer/Modules/UserInterface.lua
@@ -0,0 +1,33 @@
+local UI = {
+
+	["ICONS"] = {
+		["TOPBAR"] = {
+			["CONTROLMENU"] = {
+				
+				["RESTOREDOWN-MAXIMIZE"] = {
+					["ASSET-ID"] = "http://www.roblox.com/asset/?id=6031094681",
+					["COLORS"] = {
+						-- Varies as for UI Themes.
+						["Default"] = Color3.fromRGB(25, 20, 20),
+						["Dark"] = Color3.fromRGB(255, 255, 255),
+						["Spotify"] = Color3.fromRGB(30, 215, 96),
+					}
+				},
+				
+				["CLOSE"] = {
+					["ASSET-ID"] = "http://www.roblox.com/asset/?id=6031094678",
+					["COLORS"] = {
+						-- Varies as for UI Themes.
+						["Default"] = Color3.fromRGB(25, 20, 20),
+						["Dark"] = Color3.fromRGB(255, 255, 255),
+						["Spotify"] = Color3.fromRGB(30, 215, 96),
+					}
+				},
+				
+				
+			},
+		},
+	},
+}
+
+return UI
