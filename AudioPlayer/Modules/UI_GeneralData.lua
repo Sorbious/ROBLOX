154083f39d32e9de29216b6d23a0ffcae2f426dd
Sorbious <87640739+Sorbious@users.noreply.github.com>
2021-07-21 20:32:49 +0800

    Update UI_GeneralData.lua

--- AudioPlayer/Modules/UI_GeneralData.lua
+++ AudioPlayer/Modules/UI_GeneralData.lua
@@ -5,7 +5,7 @@ local UI = {
 			["CONTROLMENU"] = {
 				
 				["RESTOREDOWN-MAXIMIZE"] = {
-					["ASSET-ID"] = "http://www.roblox.com/asset/?id=6031094681",
+					["ASSET-ID"] = "6031094681",
 					["COLORS"] = {
 						-- Varies as for UI Themes.
 						["Default"] = Color3.fromRGB(25, 20, 20),
@@ -15,7 +15,7 @@ local UI = {
 				},
 				
 				["CLOSE"] = {
-					["ASSET-ID"] = "http://www.roblox.com/asset/?id=6031094678",
+					["ASSET-ID"] = "6031094678",
 					["COLORS"] = {
 						-- Varies as for UI Themes.
 						["Default"] = Color3.fromRGB(25, 20, 20),
